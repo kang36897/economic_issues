64bbb2d0f6447b938d9a5f86b07e1d44b0c31b56
Brian Kang <kang36897@gmail.com>
2019-07-01 21:53:08 +0800

    1.do not combine all the result into one; 2.insert every piece of data once it is available

--- data_processing.py
+++ data_processing.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     db_config = config_data["db_config"]
     db_saver = DBSaver.createSaver(db_config, cook.getInvolvedSignals())
 
-    savers = [csv_saver, db_saver]
+    savers = [db_saver]
 
     sieve = None
     if 'filter' in config_data:
